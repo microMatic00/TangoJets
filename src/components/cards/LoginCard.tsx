10a8a8012d06be3b3e222296d5f77bcde002ffcc
JustBecerra <justj.becerra@gmail.com>
2024-10-08 15:51:18 -0300

    add visual changes

--- src/components/cards/LoginCard.tsx
+++ src/components/cards/LoginCard.tsx
@@ -1,13 +1,20 @@
 export const LoginCard = () => {
 	return (
-		<div className="flex flex-col w-full">
-			<h1 className="text-4xl font-bold">Login</h1>
-			<div className="flex flex-col w-full">
-				<p className="text-lg  text-gray-200">Email</p>
-				<input placeholder="Enter Email" />
-				<p className="text-lg text-gray-200">Password</p>
-				<input placeholder="Enter Password" />
+		<div className="flex flex-col w-full items-center">
+			<h1 className="text-4xl font-bold mb-6">Welcome!</h1>
+			<div className="flex flex-col gap-4 w-full">
+				<input
+					className="w-full rounded-full px-4 py-2 text-black"
+					placeholder="Enter Email"
+				/>
+				<input
+					className="w-full rounded-full px-4 py-2 text-black"
+					placeholder="Enter Password"
+				/>
 			</div>
+			<button className="mt-6 border-2 border-gray-200 border-solid w-fit rounded-full px-4 py-2">
+				Login
+			</button>
 		</div>
 	)
 }
