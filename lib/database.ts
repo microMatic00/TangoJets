dbad2a775793e931b22b9d58a3540e947efaf3b5
NicoDeca <85821406+NicoDeca@users.noreply.github.com>
2024-10-11 20:26:44 -0300

    cree componente table para reutilizar, agregue modaladdjet

--- lib/database.ts
+++ lib/database.ts
@@ -1,8 +1,18 @@
 // lib/database.js
 export async function getClients() {
     return [
-      { id: 1, nombre: "Juan", apellido: "Pérez", telefono: "123456789", email: "juan.perez@example.com", idPasaporte: "A12345678" },
-      { id: 2, nombre: "María", apellido: "González", telefono: "987654321", email: "maria.gonzalez@example.com", idPasaporte: "B87654321" },
+      { id: 1, firstname: "Juan", lastname: "Pérez", phonenumber: "123456789", email: "juan.perez@example.com", identification: "A12345678" },
+      { id: 2, firstname: "María", lastname: "González", phonenumber: "987654321", email: "maria.gonzalez@example.com", identification: "B87654321" },
       // Agrega más clientes ficticios según sea necesario
+    ];
+  }
+
+  export async function getJets() {
+    return [
+      { id: 1, name: "Jet Falcon", capacity: "12", flightrange:"5000 km", speed: "850 km/h", price: "$35M" },
+      { id: 2, name: "Gulfstream G700", capacity: "19", flightrange:"7500 km", speed: "950 km/h", price: "$75M" },
+      { id: 3, name: "Bombardier Global 7500", capacity: "19", flightrange:"7700 km", speed: "950 km/h", price: "$72M" },
+      { id: 4, name: "Dassault Falcon 8X", capacity: "16", flightrange:"6450 km", speed: "900 km/h", price: "$58M" },
+      { id: 5, name: "Embraer Praetor 600", capacity: "12", flightrange:"4400 km", speed: "860 km/h", price: "$21M" },
     ];
   }
