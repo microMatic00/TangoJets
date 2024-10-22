57f65fae8786180029caf60d88dd15c4ba873055
NicoDeca <85821406+NicoDeca@users.noreply.github.com>
2024-10-22 01:13:41 -0300

    menti, ahora se puede eliminar cliente, flight y airship

--- src/components/buttons/DeleteEdit.tsx
+++ src/components/buttons/DeleteEdit.tsx
@@ -16,6 +16,9 @@ const DeleteEdit = ({ id, url }: { id: number; url: string }) => {
       }
       console.log(`Cliente con ID ${id} eliminado`);
       // Aquí puedes agregar lógica para actualizar la UI después de eliminar el cliente
+
+      window.location.reload();
+
     } catch (error) {
       console.error('Error:', error);
     }
@@ -51,7 +54,7 @@ const DeleteEdit = ({ id, url }: { id: number; url: string }) => {
           <div className="text-center">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you sure you want to delete this Client?
+                Are you sure you want to delete this id {id}? {/*  {"cambiar a nombre de la entidad"} */}
             </h3>
             <div className="flex justify-center gap-4">
               <Button className="bg-red-600" onClick={handleDelete}>
