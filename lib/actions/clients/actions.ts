5c55a35a20738d0c05ad880ac2fc386619fbdcd7
NicoDeca <85821406+NicoDeca@users.noreply.github.com>
2024-10-23 01:03:49 -0300

    ya se puede eliminar con react cli,fli,air y ademas se puede add cli

--- lib/actions/clients/actions.ts
+++ lib/actions/clients/actions.ts
@@ -1,1 +1,64 @@
-//
+export async function getClients() {
+	try {
+		const response = await fetch(
+			`${import.meta.env.PUBLIC_BACKEND_URL}/clients`
+		)
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`)
+		}
+
+		const responseToken = await response.json()
+		console.log("dentro de actions", responseToken)
+		return responseToken
+	} catch (err) {
+		console.error("Error fetching clients:", err)
+		throw err
+	}
+}
+
+export async function addClient(clientData: Record<string, any>) {
+    try {
+        const response = await fetch(`${import.meta.env.PUBLIC_BACKEND_URL}/client`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(clientData),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Verificar si la respuesta tiene contenido antes de intentar parsear como JSON
+        const responseText = await response.text();
+        const responseData = responseText ? JSON.parse(responseText) : {};
+
+        return responseData;
+    } catch (err) {
+        console.error("Error adding client:", err);
+        throw err;
+    }
+}
+// export async function addClient(clientData: Record<string, any>) {
+//     try {
+//         const response = await fetch(`${import.meta.env.PUBLIC_BACKEND_URL}/client`, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(clientData),
+//         });
+
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const responseData = await response.json();
+//         return responseData;
+//     } catch (err) {
+//         console.error("Error adding client:", err);
+//         throw err;
+//     }
+// }
