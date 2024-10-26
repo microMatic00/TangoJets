535a113bc1cd201ab405299cd72581c676d4debd
JustBecerra <justj.becerra@gmail.com>
2024-10-26 15:55:20 -0300

    remove unnecessary columns on flights tableModal

--- lib/actions/flights/actions.ts
+++ lib/actions/flights/actions.ts
@@ -9,7 +9,7 @@ export async function getFlights() {
 		}
 
 		const responseToken = await response.json()
-		console.log("dentro de actions", responseToken)
+
 		return responseToken
 	} catch (err) {
 		console.error("Error fetching flights:", err)
